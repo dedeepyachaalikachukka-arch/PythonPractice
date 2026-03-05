c4565677a5969ca503580950e1363e1f806f76e0
chaalika123 <80214363+chaalika123@users.noreply.github.com>
2026-03-05 17:04:46 -0600

    added hashmap.py

--- hashmap_dict.py
+++ hashmap_dict.py
@@ -1,7 +1,11 @@
 my_dict = {'a' : 5, 'b' : 2, 'c' : 8, 'd' : 1}
+my_nodict = {'p' : 10, 'q' : 7, 'r' : 3, 's' : 14}
 
 sorted_dict_A = dict(sorted(my_dict.items(), key=lambda item: item[1]))
 sorted_dict_D = dict(sorted(my_dict.items(), key=lambda item: item[1], reverse=True))
+
+for key, value in sorted(my_nodict.items(), key=lambda item: item[1]):
+    print(key, value)
 
 print("Ascending: " ,sorted_dict_A)
 print("Descending: " ,sorted_dict_D)
